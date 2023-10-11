aae8a840644fcf2197360c1d7ea6286c9ca3d0ab
yemingzhi <2275843137@qq.com>
2023-10-11 18:40:42 +0800

    实现Sequential，sigmoid的求导有点问题

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 import pandas as pd
 from homework1 import data_washing
 import numpy as np
-from model import Linear
+from model import Linear, Sequential, Sigmoid
 
 if __name__ == "__main__":
     # 数据读取
@@ -14,17 +14,18 @@ if __name__ == "__main__":
     lungs = data_washing(data['肺活量'].values)
     gender = data['性别'].values
     gender = gender > 0
-
+    torch.manual_seed(2023)
     training_data = torch.tensor(torch.from_numpy(
         np.concatenate([shoes[..., None], height[..., None], weight[..., None], race[..., None], lungs[..., None]],
                        axis=-1)).clone().detach().requires_grad_(True), dtype=torch.float32)
-    training_data=(training_data-training_data.mean(0,keepdim=True))/training_data.var(0,keepdim=True)
+    training_data = (training_data - training_data.mean(0, keepdim=True)) / training_data.var(0, keepdim=True)
     label = torch.from_numpy(gender).to(torch.float32)
-    net = Linear(5, 1)
+    net = Sequential([Linear(5, 5), Linear(5, 5), Linear(5, 1)])
 
-    for i in range(200):
+    for i in range(100):
+        training_data = training_data[torch.randperm(training_data.shape[0])]
         net.zero_grad()
-        out = torch.sigmoid(net.forward(training_data))
-        print(torch.mean((out - label.reshape(out.shape))**2))
-        net.backward((out - label.reshape(out.shape))*out*(1-out))
+        out = net.forward(training_data.unsqueeze(-1))
+        print(torch.mean((out - label.reshape(out.shape)) ** 2))
+        net.backward((out - label.reshape(out.shape)).transpose(1, 2))
         net.step()
--- model.py
+++ model.py
@@ -5,14 +5,58 @@ class BasicModel:
     def __init__(self):
         self.w = None
         self.bias = None
-        self.mean = None
-        self.var = None
+        self.forward_value = None
 
     def forward(self, input):
         pass
 
     def backward(self, input):
         pass
+
+
+class BasicActivation:
+    def __init__(self):
+        pass
+
+    def forward(self, input):
+        pass
+
+    def backward(self, input):
+        pass
+
+    def zero_grad(self):
+        pass
+
+    def step(self, lr=0.001):
+        pass
+
+
+class BasicNorm:
+    def __init__(self):
+        self.mean = None
+        self.var = None
+        self.forward_value = None
+
+    def forward(self, input):
+        pass
+
+    def backward(self, input):
+        pass
+
+    def step(self, lr=0.001):
+        pass
+
+
+class Sigmoid(BasicActivation):
+    def __init__(self):
+        super(Sigmoid, self).__init__()
+
+    def forward(self, input):
+        self.forward_value = 1 / (1 + torch.exp(-input))
+        return self.forward_value
+
+    def backward(self, input):
+        return input * self.forward_value.transpose(1, 2) * (1 - self.forward_value.transpose(1, 2))
 
 
 class Linear(BasicModel):
@@ -23,7 +67,6 @@ class Linear(BasicModel):
         self.w = torch.rand([output, input], dtype=torch.float32, requires_grad=True)
         if bias:
             self.b = torch.rand([output, 1], dtype=torch.float32, requires_grad=True)
-        self.forward_value = None
 
     def zero_grad(self):
         self.w.grad = torch.ones_like(self.w)
@@ -34,7 +77,7 @@ class Linear(BasicModel):
         self.batchsize, self.input_channel = input.shape[0], input.shape[1]
         w = self.w.repeat(self.batchsize, 1, 1)
         b = self.b.repeat(self.batchsize, 1, 1)
-        out = torch.bmm(w, input.unsqueeze(-1))
+        out = torch.bmm(w, input)
         self.forward_value = input
         return out + b
 
@@ -42,10 +85,36 @@ class Linear(BasicModel):
         if input is None:
             self.w.grad = self.forward_value.mean(0, keepdim=True) + self.w.grad
             self.b.grad = torch.ones_like(self.b.grad)
+            return self.w.repeat(self.batchsize, 1, 1)
         else:
-            self.w.grad = torch.bmm(input, self.forward_value.unsqueeze(-2)).mean(0) + self.w.grad
-            self.b.grad = input.mean(0)
+            self.w.grad = torch.bmm(input.transpose(1, 2), self.forward_value.transpose(1, 2)).mean(0) + self.w.grad
+            self.b.grad = input.mean(0).T
+            return torch.bmm(input, self.w.repeat(self.batchsize, 1, 1))
 
     def step(self, lr=0.001):
         self.w.data -= lr * self.w.grad
         self.b.data -= lr * self.b.grad
+
+
+class Sequential:
+    def __init__(self, input=[]):
+        self.model_list = input
+
+    def forward(self, input):
+        x = input
+        for i in range(len(self.model_list)):
+            x = self.model_list[i].forward(x)
+        return x
+
+    def step(self, lr=0.001):
+        for i in range(len(self.model_list)):
+            self.model_list[i].step(lr)
+
+    def zero_grad(self):
+        for i in range(len(self.model_list)):
+            self.model_list[i].zero_grad()
+
+    def backward(self, input=None):
+        x = input
+        for i in reversed(range(len(self.model_list))):
+            x = self.model_list[i].backward(x)
